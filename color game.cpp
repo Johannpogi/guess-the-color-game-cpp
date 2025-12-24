ed09e4cbeb76688ad8cf03cc58bf0c7cc0d03b7a
Jonathan Cabiling Domingo <162078328+Johannpogi@users.noreply.github.com>
2025-12-24 10:12:48 +0800

    Add files via upload

---
+++ color game.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string color = "Blue";	
+	string guess;
+	
+    cout << "Guess the Color: " << endl;
+    
+    for(int x = 0; x < 5; x++){
+    	
+    	cout << "Heart: " << 5 - x << endl;
+    	cin >> guess;
+    	
+    	if(guess == color){
+    		cout << "You are Correct!" << endl;
+    		break;
+		}
+		else{ 
+			cout << "Incorrect Guess!" << endl;
+		}
+		if (x == 4){
+			cout << "Game Over!" << endl;
+		}	
+	}
+	return 0;
+}
