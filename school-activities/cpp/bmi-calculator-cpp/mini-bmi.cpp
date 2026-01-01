a59163f4aa1dbc5222c52c7688f9ff4be11207a0
Jonathan Cabiling Domingo <162078328+Johannpogi@users.noreply.github.com>
2026-01-01 23:21:38 +0800

    Enhance BMI calculator with input validation
    Added input validation for weight and height, and updated BMI category checks.

--- school-activities/cpp/bmi-calculator-cpp/mini-bmi.cpp
+++ school-activities/cpp/bmi-calculator-cpp/mini-bmi.cpp
@@ -3,22 +3,26 @@ using namespace std;
 
 int main () {
     float weight, height, bmi;
+    cout << "BMI Categories: Underweight <18.5, Normal 18.5-24.9, Overweight 25-29.9, Obese >=30" << endl;
     cout << "Your weight in kg: " << endl;
     cin >> weight;
     cout << "your height in meter: " << endl;
     cin >> height;
     bmi = weight / (height * height);
-
-    if (bmi < 18) {
+    
+   if (weight <= 0 || height <= 0) {
+    cout << "Invalid input! Weight and height must be positive numbers." << endl;
+   }
+    else if (bmi < 18.5) {
         cout << "You are underweight: " << bmi << endl;
     }
-    else if (18 <= bmi && bmi <= 24.9) {
+    else if (bmi <= 24.9) {
         cout << "You are normal: " << bmi << endl; 
     }
-    else if (bmi > 24.9 && bmi <= 29.9) {
+    else if (bmi <= 29.9) {
         cout << "You are overweight: " << bmi  << endl;
     }
-    else if (bmi >= 30) {
+    else {
         cout << "You are obese, you need to exercise: " << bmi << endl;
     }
     return 0;
